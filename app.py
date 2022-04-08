6764b4b39e031cced497424ff0a45e56f049a5f3
IT ONE MM <itonemm@gmail.com>
2022-04-08 16:04:18 +0630

    with logging

--- app.py
+++ app.py
@@ -2,28 +2,32 @@ from datetime import timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 from date_utils import get_date, myanmar_timezone
 import logging
+import time
+
 
 count = 0
 
 logging.basicConfig()
-logging.getLogger('apscheduler').setLevel(logging.DEBUG)
-
-sched = BackgroundScheduler(timezone=myanmar_timezone)
+logger = logging.getLogger('apscheduler')
+logger.setLevel(logging.DEBUG)
 
 def tick():
-    print('Tick! The time is: %s' % get_date().get('datetime'))
+    logger.info('Tick! The time is: %s' % get_date().get('datetime'))
+    logger.debug('Tick! The time is: %s' % get_date().get('datetime'))
+    time.sleep(2)
     count += 1
     if count == 5:
       sched.remove_job('my_job')
       sched.shutdown()
 
+
 if __name__ == '__main__':
-  
-  # now = get_date().get('date') + timedelta(seconds=5)
+
+  sched = BackgroundScheduler(timezone=myanmar_timezone)
 
   sched.add_job(tick, 'interval', seconds=5, id='my_job', timezone=myanmar_timezone)
 
   try:
     sched.start()
   except (KeyboardInterrupt, SystemExit):
-    pass
+    logger.exception('Something went wrong.')

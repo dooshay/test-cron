3749199d3804654c79b1e462f6a43e24044e1790
IT ONE MM <itonemm@gmail.com>
2022-04-08 16:44:56 +0630

    change blocking scheduler

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from date_utils import get_date, myanmar_timezone
 # import logging
 import time
@@ -22,7 +22,7 @@ def tick():
 
 if __name__ == '__main__':
 
-  sched = BackgroundScheduler(timezone=myanmar_timezone)
+  sched = BlockingScheduler(timezone=myanmar_timezone)
 
   sched.add_job(tick, 'interval', seconds=5, id='my_job', timezone=myanmar_timezone)
 

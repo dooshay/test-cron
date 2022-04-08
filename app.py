f50e17c75734fa84fb646f930e72a7b878007a6a
IT ONE MM <itonemm@gmail.com>
2022-04-08 16:51:18 +0630

    haha

--- app.py
+++ app.py
@@ -11,23 +11,21 @@ import time
 # logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 def tick():
-    print('Tick! The time is: %s', get_date().get('datetime'))
-    time.sleep(2)
-    count += 1
-    if count == 5:
-      sched.remove_job('my_job')
-      sched.shutdown()
+  print('Tick! The time is: %s', get_date().get('datetime'))
+  time.sleep(2)
+  count += 1
+  if count == 5:
+    sched.remove_job('my_job')
+    sched.shutdown()
 
 
-if __name__ == '__main__':
+count = 0
 
-  count = 0
+sched = BlockingScheduler(timezone=myanmar_timezone)
 
-  sched = BlockingScheduler(timezone=myanmar_timezone)
+sched.add_job(tick, 'interval', seconds=5, id='my_job', timezone=myanmar_timezone)
 
-  sched.add_job(tick, 'interval', seconds=5, id='my_job', timezone=myanmar_timezone)
-
-  try:
-    sched.start()
-  except (KeyboardInterrupt, SystemExit):
-    print('Something went wrong.')
+try:
+  sched.start()
+except (KeyboardInterrupt, SystemExit):
+  print('Something went wrong.')
